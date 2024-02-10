41724377e25396a3f91c9da883dc1a8d423aac62
Valery Pronin <pownmymail@gmail.com>
2024-02-10 21:11:07 +0300

    feat: Add custom fields to CTA

--- template-parts/content/section-cta.php
+++ template-parts/content/section-cta.php
@@ -1,16 +1,22 @@
+<?php
+$image = get_sub_field('image');
+$heading = get_sub_field('heading');
+$link = get_sub_field('link');
+?>
+
 <!-- CTA -->
 <section class="py-48 relative">
   <!-- BG -->
   <picture class="">
-    <img src="http://happywheels.local/wp-content/uploads/2024/02/IMG_1055.jpg" class="absolute top-0 left-0 w-full h-full object-cover object-center" alt="Denis Novozilov Dubai HappyWheels Roller Classes">
+    <img src="<?= $image['url'] ?>" class="absolute top-0 left-0 w-full h-full object-cover object-center" alt="Denis Novozilov Dubai HappyWheels Roller Classes">
   </picture>
   <div class="wrapper">
     <div class="box relative flex flex-col items-center">
-      <h2 class="heading mb-16 text-white">
-        То что нужно?
+      <h2 class="heading mb-16 text-white text-center">
+        <?= $heading ?>
       </h2>
-      <a href="#" class="button">
-        погнали
+      <a href="<?= $link['url'] ?>" class="button">
+        <?= $link['title'] ?>
       </a>
     </div>
   </div>
